1a8e4b4685caeca6416f8cb7e886b490dce5f45c
Sharef184 <sharifkhlief@gmail.com>
2019-09-16 01:11:27 +0200

    Updates

---
+++ precache-manifest.41131b07f30d4e84e16840047d024971.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e71699b95a3fcfe8a7e85bcfd24c0c4",
+    "url": "/WIW-Finder/index.html"
+  },
+  {
+    "revision": "17aeabfa939961f1c1bc",
+    "url": "/WIW-Finder/static/css/main.77b496d1.chunk.css"
+  },
+  {
+    "revision": "4bf2a816be21799aecd2",
+    "url": "/WIW-Finder/static/js/2.3d3626b9.chunk.js"
+  },
+  {
+    "revision": "17aeabfa939961f1c1bc",
+    "url": "/WIW-Finder/static/js/main.373b700a.chunk.js"
+  },
+  {
+    "revision": "1d46b50cb2ecf31c32b2",
+    "url": "/WIW-Finder/static/js/runtime~main.c65c891c.js"
+  }
+]);
